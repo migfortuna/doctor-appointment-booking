205095631059970859423f022f9142b4c4795992
Miguel Fortuna <migfortuna@Miguels-MacBook-Air.local>
2024-10-04 16:04:58 +0800

    10/04 Update 1. Created separate edit button component for the profile page 2. Finished the profile page to enable editing account details

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   MdOutlineEmail,
   MdOutlinePhoneIphone,
@@ -6,63 +6,166 @@ import {
 } from "react-icons/md";
 import { AppContext } from "../context";
 import { assets } from "../assets/assets.js";
+import EditButton from "../components/Profile/EditButton.jsx";
 
 const Profile = () => {
-  const { currentUser } = useContext(AppContext);
+  const { currentUser, setCurrentUser } = useContext(AppContext);
+  const [isEditable, setIsEditable] = useState(false);
+  const [userData, setUserData] = useState({
+    email: currentUser?.email,
+    // firstName: currentUser?.firstName,
+    // lastName: currentUser?.lastName,
+    phone: currentUser?.phone,
+    address: currentUser?.address,
+    gender: currentUser?.gender,
+    birthMonth: currentUser?.birthMonth,
+    birthDay: currentUser?.birthDay,
+    birthYear: currentUser?.birthYear,
+  });
+
+  const updateProfile = async (event) => {
+    setIsEditable(false);
+    event.preventDefault();
+    const res = await fetch("http://localhost:8000/api/account", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:5173",
+      },
+      body: JSON.stringify(userData),
+    });
+    const { error, data } = await res.json();
+    if (error) {
+      console.log("ERROR", error);
+    } else {
+      setCurrentUser({ ...data });
+      setUserData({
+        email: data.email,
+        // firstName: data.firstName,
+        // lastName: data.lastName,
+        phone: data.phone,
+        address: data.address,
+        gender: data.gender,
+        birthMonth: data.birthMonth,
+        birthDay: data.birthDay,
+        birthYear: data.birthYear,
+      });
+    }
+  };
+
   return (
     <section className="min-h-[80vh] flex flex-col items-center">
-      <div
+      <form
+        action=""
         className="flex flex-col items-start gap-4 
         m-auto p-8 sm:min-w-96 border rounded-xl
         text-zinc-600 text-sm shadow-lg"
+        onSubmit={(e) => updateProfile(e)}
       >
         <img
           src={assets.profile_pic}
           alt="profile-pic"
           className="rounded-md m-auto"
         />
-        <h1 className="text-2xl font-medium">
-          {currentUser.firstName} {currentUser.lastName}
-        </h1>
+        <div className="w-full flex flex-row justify-between items-center">
+          <h1 className="text-xl md:text-2xl font-medium">
+            {userData.firstName} {userData.lastName}
+          </h1>
+          <EditButton isEditable={isEditable} setIsEditable={setIsEditable} />
+        </div>
+
         <div className="w-full border-[0.05px] border-gray-300"></div>
 
         <div className="w-full flex flex-col gap-3 mb-3">
           <p className="uppercase underline">contact information</p>
           <div className="flex justify-between items-center">
             <MdOutlineEmail size={15} />
-            <p>{currentUser.email}</p>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              disabled
+              value={userData.email}
+              className="bg-white text-end"
+              // defaultValue={userData.email}
+              // disabled={!isEditable}
+              // className={`bg-white text-end ${
+              //   isEditable && "border border-gray-300 rounded-md px-2"
+              // }`}
+              // onChange={(e) =>
+              //   setUserData({ ...userData, email: e.target.value })
+              // }
+            />
           </div>
           <div className="flex justify-between items-center">
             <MdOutlinePhoneIphone size={15} />
-            <p className={`${!currentUser.phone && "hidden"}`}>
-              {currentUser.phone}
-            </p>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              defaultValue={userData.phone}
+              disabled={!isEditable}
+              className={`bg-white text-end ${
+                isEditable && "border border-gray-300 rounded-md px-2"
+              }`}
+              onChange={(e) =>
+                setUserData({ ...userData, phone: e.target.value })
+              }
+            />
           </div>
-          <div className="flex justify-between items-center">
+          <div className="flex flex-wrap justify-between items-center">
             <MdOutlinePinDrop size={15} />
-            <p className={`${!currentUser.address && "hidden"}`}>
-              {currentUser.address}
-            </p>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              defaultValue={userData.address}
+              disabled={!isEditable}
+              className={`bg-white text-end ${
+                isEditable && "border border-gray-300 rounded-md px-2"
+              }`}
+              onChange={(e) =>
+                setUserData({ ...userData, address: e.target.value })
+              }
+            />
           </div>
         </div>
 
         <div className="w-full flex flex-col gap-3">
           <p className="uppercase underline">basic information</p>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <p>Gender:</p>
-            <p className={`${!currentUser.gender && "hidden"}`}>
-              {currentUser.gender}
-            </p>
+            <input
+              type="text"
+              id="gender"
+              name="gender"
+              defaultValue={userData.gender}
+              disabled={!isEditable}
+              className={`bg-white text-end ${
+                isEditable && "border border-gray-300 rounded-md px-2"
+              }`}
+              onChange={(e) =>
+                setUserData({ ...userData, gender: e.target.value })
+              }
+            />
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <p>Birthday:</p>
-            <p className={`${!currentUser.birthMonth && "hidden"}`}>
-              {currentUser.birthDay} {currentUser.birthMonth},{" "}
-              {currentUser.birthYear}
+            <p className={`${!userData.birthMonth && "hidden"}`}>
+              {userData.birthDay} {userData.birthMonth}, {userData.birthYear}
             </p>
           </div>
         </div>
-      </div>
+
+        <button
+          type="submit"
+          className={`${
+            !isEditable && "hidden"
+          } border border-gray-300 mt-3 px-7 py-1 rounded-full ms-auto`}
+        >
+          Save
+        </button>
+      </form>
     </section>
   );
 };
